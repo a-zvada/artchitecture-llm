28c96acfcadb3b9275e03ef08114101a12db4070
CRAY\Alessandro <zvada@mail.ru>
2025-11-25 22:13:58 +0300

    Task 3 / 1

--- build_index.py
+++ build_index.py
@@ -30,12 +30,12 @@ splits = text_splitter.split_documents(docs)
 print(f"Разбито на {len(splits)} чанков")
 
 model_name = "BAAI/bge-m3"
-model_kwargs = {"device": "cuda"}
+#model_kwargs = {"device": "cuda"}
 encode_kwargs = {"normalize_embeddings": True}
 
 embeddings = HuggingFaceEmbeddings(
     model_name=model_name,
-    model_kwargs=model_kwargs,
+#    model_kwargs=model_kwargs,
     encode_kwargs=encode_kwargs
 )
 
